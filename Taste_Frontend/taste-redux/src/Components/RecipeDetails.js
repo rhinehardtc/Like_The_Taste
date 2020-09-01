8f07841369660daf19f39ccdf91824521d5229ec
rhinehardtc <rhinehardtc@gmail.com>
2020-09-01 18:38:44 -0500

    added overflow scroll

--- Taste_Frontend/taste-redux/src/Components/RecipeDetails.js
+++ Taste_Frontend/taste-redux/src/Components/RecipeDetails.js
@@ -4,7 +4,7 @@ import '../Styles/RecipeDetails.css';
 const RecipeDetails = (props) => {
     const {id, title, time, description, steps, img_url} = props.recipe
     return(
-        <div className="recipe_div" >
+        <div className="recipe_details_div" >
             {console.table(props.recipe.ingredients)}
             {console.table(props.recipe.tags)}
             <h1 className="recipe_title">{title}</h1>
